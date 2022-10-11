821905538fa0bd0845204abb9feac0f4a6c9d89d
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2022-10-11 16:27:35 -0700

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog_122_F23_Lecture_7_WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Prog_122_F22_Lecture_7_WinForms
+ Programming 122 - F22 - Lecture 7 - WinForms
